1dec51a6d4275629b0f8e8b1bfb0a5f113cdaaad
Sanmit Suthar <sanmitsuthar3@gmail.com>
2024-09-09 15:27:32 +0530

    Adjust loading animation position in playground

--- src/components/play/Play.jsx
+++ src/components/play/Play.jsx
@@ -1,5 +1,3 @@
-// Play.jsx
-
 import React, { useState, useEffect } from "react";
 import Spline from "@splinetool/react-spline";
 import "./play.css";
@@ -22,7 +20,7 @@ function Play() {
       setSceneUrl(url);
     };
 
-    loadScene();
+    // loadScene();
   }, []);
 
   const handleLoad = () => {
@@ -31,73 +29,73 @@ function Play() {
 
   return (
     <section className="play" id="play">
-    {isLoading && (
-      <div className="loading_animation">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
-          <circle
-            fill="var(--first-color)"
-            stroke="var(--first-color)"
-            strokeWidth="15"
-            r="15"
-            cx="40"
-            cy="100"
-          >
-            <animate
-              attributeName="opacity"
-              calcMode="spline"
-              dur="2s"
-              values="1;0;1;"
-              keySplines=".5 0 .5 1;.5 0 .5 1"
-              repeatCount="indefinite"
-              begin="-.4s"
-            />
-          </circle>
-          <circle
-            fill="var(--first-color)"
-            stroke="var(--first-color)"
-            strokeWidth="15"
-            r="15"
-            cx="100"
-            cy="100"
-          >
-            <animate
-              attributeName="opacity"
-              calcMode="spline"
-              dur="2s"
-              values="1;0;1;"
-              keySplines=".5 0 .5 1;.5 0 .5 1"
-              repeatCount="indefinite"
-              begin="-.2s"
-            />
-          </circle>
-          <circle
-            fill="var(--first-color)"
-            stroke="var(--first-color)"
-            strokeWidth="15"
-            r="15"
-            cx="160"
-            cy="100"
-          >
-            <animate
-              attributeName="opacity"
-              calcMode="spline"
-              dur="2s"
-              values="1;0;1;"
-              keySplines=".5 0 .5 1;.5 0 .5 1"
-              repeatCount="indefinite"
-              begin="0s"
-            />
-          </circle>
-        </svg>
-        Please wait....
-      </div>
-    )}
-    {sceneUrl && (
-      <Spline
-        scene={sceneUrl}
-        onLoad={handleLoad} // Triggered when the Spline scene is fully loaded
-      />
-    )}
+      {isLoading && (
+        <div className="loading_animation">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+            <circle
+              fill="var(--first-color)"
+              stroke="var(--first-color)"
+              strokeWidth="15"
+              r="15"
+              cx="40"
+              cy="100"
+            >
+              <animate
+                attributeName="opacity"
+                calcMode="spline"
+                dur="2s"
+                values="1;0;1;"
+                keySplines=".5 0 .5 1;.5 0 .5 1"
+                repeatCount="indefinite"
+                begin="-.4s"
+              />
+            </circle>
+            <circle
+              fill="var(--first-color)"
+              stroke="var(--first-color)"
+              strokeWidth="15"
+              r="15"
+              cx="100"
+              cy="100"
+            >
+              <animate
+                attributeName="opacity"
+                calcMode="spline"
+                dur="2s"
+                values="1;0;1;"
+                keySplines=".5 0 .5 1;.5 0 .5 1"
+                repeatCount="indefinite"
+                begin="-.2s"
+              />
+            </circle>
+            <circle
+              fill="var(--first-color)"
+              stroke="var(--first-color)"
+              strokeWidth="15"
+              r="15"
+              cx="160"
+              cy="100"
+            >
+              <animate
+                attributeName="opacity"
+                calcMode="spline"
+                dur="2s"
+                values="1;0;1;"
+                keySplines=".5 0 .5 1;.5 0 .5 1"
+                repeatCount="indefinite"
+                begin="0s"
+              />
+            </circle>
+          </svg>
+          <p>Please wait...</p>
+          </div>
+      )}
+      {sceneUrl && (
+        <Spline
+          scene={sceneUrl}
+          onLoad={handleLoad} // Triggered when the Spline scene is fully loaded
+        />
+      )}
     </section>
   );
 }
