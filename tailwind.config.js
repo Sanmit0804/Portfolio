61ddd391eb8263c67ae51017560bdf0ff6119c71
Sanmit Suthar <sanmitsuthar3@gmail.com>
2024-11-07 17:30:29 +0530

    add icon cloud in about me section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {},
   },
